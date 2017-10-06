6c12d69b99c2656e61c741bedf9c731d87b9e6db
szubersk <szuberskidamian@gmail.com>
2017-10-06 19:11:50 +0800

    refactored argument parsing

--- poker_stats.py
+++ poker_stats.py
@@ -1,30 +1,14 @@
 #!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
 
 import getopt
 import logging
 import operator
 import sys
 
-import entity
-import report
-
-def usage():
-    print '''
-Usage:
-$ poker_stats.py [ARGS] [FILE]
-
-Program arguments (ARGS):
-    -p, --player: Name of the player, argument is mandatory
-    -f, --position: Filter hands by position (BTN, SB, BB, UTG, MP, CO)
-    -s, --sort: Sort hands by pot size
-    -v, --voluntary: Filter hands which were played voluntarily
-    -d, --dump: Dump hands in PokerStars format
-'''
-
-def fail(msg):
-    logging.error(msg)
-    usage()
-    sys.exit(1)
+import poker_stats.config as config
+import poker_stats.entity as entity
+import poker_stats.report as report
 
 def parseHandsFromFile(filename, player):
     result = []
@@ -36,7 +20,12 @@ def parseHandsFromFile(filename, player):
     for line in lines:
         if line == '\r\n':
             if handInProcess:
+                hand.lines[0].replace('PokerStars Zoom Hand', 'PokerStars Hand')
+                hand.lines.append('\r\n')
+                hand.lines.append('\r\n')
+                hand.lines.append('\r\n')
                 hand.parse(player)
+
                 if hand.position != None:
                     result.append(hand)
                 handInProcess = False
@@ -49,73 +38,22 @@ def parseHandsFromFile(filename, player):
 
     return result
 
-def parse_arguments(argv):
-    if len(argv) == 0:
-        fail('No input arguments specified')
-
-    try:                                
-        opts, args = getopt.getopt(argv, 'p:f:svd', ['player=', 'position=', 'sort', 'voluntary', 'dump'])
-    except getopt.GetoptError as e:
-        fail(str(e))
-
-    player = None
-    sort = False
-    posfilter = None
-    voluntary = False
-    dump = False
-    for opt, arg in opts:
-        if opt in ('-p', '--player'):
-            player = arg
-        if opt in ('-f', '--position'):
-            posfilter = arg
-        if opt in ('-s', '--sort'):
-            sort = True
-        if opt in ('-v', '--voluntary'):
-            voluntary = True
-        if opt in ('-d', '--dump'):
-            dump = True
-
-    if player == None:
-        fail('Player not defined')
-
-    if posfilter not in [None, 'BTN', 'SB', 'BB', 'UTG', 'MP', 'CO']:
-        fail('Filter provided is invalid. Should one of: (BTN, SB, BB, UTG, MP, CO)')
-
-    logging.info('Configuration')
-    logging.info('Player: {}'.format(player))
-    logging.info('Position: {}'.format(posfilter))
-    logging.info('Sort: {}'.format(sort))
-    logging.info('Voluntarily enter the pot: {}'.format(voluntary))
-    logging.info('Dump hands: {}'.format(dump))
-
-    return (player, posfilter, sort, voluntary, dump, args)
-
 def dump_hands(hands):
-    result = []
-    for hand in hands:
-        result.append(hand.lines[0].replace('PokerStars Zoom Hand', 'PokerStars Hand'))
-        result.extend(hand.lines[1:])
-        result.append('\r\n')
-        result.append('\r\n')
-        result.append('\r\n')
+    sys.stdout.writelines(reduce(lambda acc, h: acc + h.lines, hands, []))
 
-    sys.stdout.writelines(result)
-
-def main(argv):
+def main():
     logging.basicConfig(level=logging.INFO, format='[ %(levelname)s ] %(message)s')
 
-    (player, position, sort, voluntary, dump, files) = parse_arguments(argv)
-
     hands = []
-    for filename in files:
+    for filename in config.args.files:
         logging.info('Parsing hands from {}'.format(filename))
-        hands.extend(parseHandsFromFile(filename, player))
+        hands.extend(parseHandsFromFile(filename, config.args.player))
     logging.info('Parsed {} hands'.format(len(hands)))
 
     hands = filter(lambda h: len(h.preflop) > 0, hands)
-    logging.info('{} participated in {} hands'.format(player, len(hands)))
+    logging.info('{} participated in {} hands'.format(config.args.player, len(hands)))
 
-    if voluntary:
+    if config.args.voluntary:
         def flt(h):
             a = h.preflop
             if 'SB' in h.position or 'BB' in h.position:
@@ -126,17 +64,17 @@ def main(argv):
         hands = filter(flt, hands)
         logging.info('Voluntarily entered the pot with {} hands'.format(len(hands)))
 
-    if position != None:
-        hands = filter(lambda hand: hand.position == position, hands)
-        logging.info('{} hands played on {}'.format(len(hands), position))
+    if config.args.position != None:
+        hands = filter(lambda hand: hand.position == config.args.position, hands)
+        logging.info('{} hands played on {}'.format(len(hands), config.args.position))
 
-    if sort:
+    if config.args.sort:
         hands.sort(key = lambda hand: hand.pot, reverse=True)
 
-    if dump:
+    if config.args.dump:
         dump_hands(hands)
 
     report.print_stats(hands)
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
---
+++ poker_stats/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--player', help='Player', required=True)
+parser.add_argument('-f', '--position', help='Position', choices=['SB', 'BB', 'UTG', 'MP', 'CO', 'BTN'])
+parser.add_argument('-v', '--voluntary', action='store_true', help='Voluntarily enter the pot')
+parser.add_argument('-d', '--dump', action='store_true', help='Dump hands')
+parser.add_argument('-s', '--sort', action='store_true', help='Sort')
+parser.add_argument('files', help='File list', nargs='+')
+
+args = parser.parse_args()
+
+del parser
