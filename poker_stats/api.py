4e27c29b782abf27d5df23602eb347f524eb365d
szubersk <szuberskidamian@gmail.com>
2018-05-06 19:02:16 +0800

    feature: added missing holding report to web API feature: API version bumped up to 0.3 bugfix: fixed position report in web API

--- poker_stats/api.py
+++ poker_stats/api.py
@@ -1,46 +1,49 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
-from flask import Flask, jsonify
+import flask
+from . import hand_filter
 from . import hand_parser
 from . import report
 
-app = Flask(__name__)
-hands = hand_parser.parse_files(['./session'])
+all_hands = hand_parser.parse_files(['./session'])
+app = flask.Flask(__name__)
 
-@app.route('/api/0.1/position_report/<player_name>', methods=['GET'])
+@app.route('/api/0.3/position_report/<player_name>', methods=['GET'])
 def api_position_report(player_name):
-    positions = ['SB', 'BB', 'UTG', 'MP', 'CO', 'BTN']
-
+    hands = hand_filter.apply_filters(all_hands, hand_filter.create({}, player_name))
     result = {}
-    for position in positions:
+    for position in ['SB', 'BB', 'UTG', 'MP', 'CO', 'BTN']:
         rep = report.create_position_report(hands, player_name, position)
         result[position] = vars(rep.profit_report)
     result['ALL'] = vars(report.create_profit_report(hands, player_name))
 
-    return jsonify(result)
+    return flask.jsonify(result)
 
-@app.route('/api/0.1/position_report/<player_name>/<position>', methods=['GET'])
+@app.route('/api/0.3/position_report/<player_name>/<position>', methods=['GET'])
 def api_position_detail_report(player_name, position):
+    hands = hand_filter.apply_filters(all_hands, hand_filter.create({}, player_name))
     rep = report.create_position_report(hands, player_name, position)
     rep_json = vars(rep)
     rep_json['profit_report'] = vars(rep.profit_report)
-    return jsonify(rep_json)
+    return flask.jsonify(rep_json)
 
-@app.route('/api/0.1/blind_report/<player_name>', methods=['GET'])
+@app.route('/api/0.3/blind_report/<player_name>', methods=['GET'])
 def api_blind_report(player_name):
+    hands = hand_filter.apply_filters(all_hands, hand_filter.create({}, player_name))
     rep = report.create_blind_report(hands, player_name)
     rep_json = vars(rep)
     rep_json['sb_report'] = vars(rep.sb_report.profit_report)
     rep_json['bb_report'] = vars(rep.bb_report.profit_report)
-    return jsonify(rep_json)
+    return flask.jsonify(rep_json)
 
-@app.route('/api/0.1/preflop_report/<player_name>', methods=['GET'])
+@app.route('/api/0.3/holding_report/<player_name>', methods=['GET'])
 def api_preflop_report(player_name):
-    rep = report.create_preflop_report(hands, player_name)
+    hands = hand_filter.apply_filters(all_hands, hand_filter.create({}, player_name))
+    rep = report.create_holding_report(hands, player_name)
     rep_json = vars(rep)
-    rep_json['profit_report'] = vars(rep.profit_report)
-    return jsonify(rep_json)
+    rep_json['stats'] = [vars(o) for o in rep.stats]
+    return flask.jsonify(rep_json)
 
 if __name__ == '__main__':
     app.run()
